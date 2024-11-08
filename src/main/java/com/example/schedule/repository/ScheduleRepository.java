9d0edf1d71c6b95e93049d08ae6e04d625ec9cff
Jeong Seon U <sw.jeong97@gmail.com>
2024-11-08 10:55:19 +0900

    feat : define ScheduleRepository interface

---
+++ src/main/java/com/example/schedule/repository/ScheduleRepository.java
@@ -0,0 +1,18 @@
+package com.example.schedule.repository;
+
+import com.example.schedule.dto.ScheduleRequestDto;
+import com.example.schedule.dto.ScheduleResponseDto;
+
+import java.util.List;
+
+public interface ScheduleRepository {
+    Long saveSchedule(ScheduleRequestDto requestDto, Long id);
+
+    List<ScheduleResponseDto> findScheduleByAuthorAndModificationDate(String author, String modificationDate);
+
+    ScheduleResponseDto findScheduleById(Long id);
+
+    ScheduleResponseDto updateSchedule(Long id, ScheduleRequestDto requestDto);
+
+    void deleteSchedule(Long id);
+}
